7d1e777e54f6bc65b33dabafbfb0af3c5f746d06
Leidy Sanchez <leysanchezb92@gmail.com>
2026-01-20 19:20:54 -0500

    new exercise for inheritance practice

---
+++ MODULO 2 POO - PATRONES/ejercicio_herencia/ejercicio_herencia.md
@@ -0,0 +1,27 @@
+## Caso
+Se requiere el diseño y desarrollo de un sistema de clases para un juego de rol (RPG, por sus siglas en inglés) que incorporará personajes, enemigos y armas. Es esencial aplicar los principios de encapsulamiento y herencia para estructurar el código de forma que resulte eficiente y fácil de mantener.
+
+ 
+
+### Instrucciones
+[x] Crea tu pequeño entorno de desarrollo con el fin de llevar a cabo esta actividad de la manera correcta.
+
+[x] Crea un archivo en Python llamado `poo_role_play.py`
+
+[x] Define una clase Personaje con atributos como nombre, nivel, y vida. Incluye un método constructor para inicializar estos atributos y métodos para recibir daño o curarse.
+
+[x] Define una clase Enemigo que herede de Personaje y añade atributos específicos como tipo (por ejemplo, zombi, vampiro) y daño.
+
+[x] Asegura que los atributos de las clases estén adecuadamente encapsulados (Define cuáles encapsular y cuáles no). Utiliza modificadores de acceso para controlar la visibilidad de los atributos y métodos, asegurando que la manipulación de los datos solo pueda hacerse a través de métodos públicos (getters y setters).
+
+[x] Crea al menos una subclase de Personaje adicional que represente otro tipo de entidad en el juego, como Aliado, con características únicas.
+
+[x] Implementa métodos que demuestren la reutilización de código a través de la herencia y la especialización de comportamientos.
+
+[x] Define una clase Arma con atributos como nombre y poder. Asocia esta clase con Personaje para que cada personaje pueda tener un arma equipada.
+
+[] Escribe un pequeño script que cree instancias de tus clases y demuestre la interacción entre ellas, como batallas entre personajes y enemigos utilizando las armas.
+
+[] Implementa interacciones entre personajes y enemigos usando las armas, por ejemplo, un método atacar que utilice el poder del arma para reducir la vida del enemigo.
+
+[x] Asegúrate de probar todos los métodos y verificaciones de encapsulamiento, especialmente el correcto funcionamiento de getters y setters.
---
+++ MODULO 2 POO - PATRONES/ejercicio_herencia/ejercicio_herencia.py
@@ -0,0 +1,180 @@
+class Weapon:
+    def __init__(self, weapon_name, weapon_power):
+        self.weapon_name = weapon_name
+        self.weapon_power = weapon_power
+
+    def attack(self):
+        print(f"Attacking with {self.weapon_name} having power {self.weapon_power}")
+
+class Character(Weapon):
+    def __init__(self, name, level, live, weapon_name, weapon_power):
+        super().__init__(weapon_name, weapon_power)
+        self.name = name
+        self._level = level
+        self._live = live
+
+    @property # Getter method level
+    def level(self):
+        return self._level
+
+    @level.setter #Setter method level
+    def level(self, value):
+        if self._level == 1 and value == 0:
+            print(f"Player {self.name} is a starter")
+        elif value <= self._level:
+            print(f"Player {self.name} can't decrease level")
+        else:
+            increase = value - self._level
+            self._level = value
+            print(f"Player {self.name} has leveled up {increase} levels")
+
+
+    @property # Getter method live
+    def live(self):
+        return self._live
+
+    @live.setter # Setter method live
+    def live(self, value):
+        if value <= 0:
+            self._live = 0
+            print(f"Player {self.name} is dead")
+        else:
+            self._live = value
+
+    def damage(self,amount):
+        self._live -= amount
+        print(f"{self.name} has taken damage!. Current health: {self.live}")
+
+    def healing(self,amount):
+        self._live += amount
+        print(f"{self.name} has been healed!. Current health: {self.live}")
+       
+
+class Enemy(Character):
+     def __init__(self, name, level, live, type_enemy, weapon_name, weapon_power=10):
+        super().__init__(name, level, live, weapon_name, weapon_power)
+        self.type_enemy = type_enemy
+
+class Allied(Character):
+    def __init__(self, name, level, live, type_ally, weapon_name, weapon_power=10 ):
+        super().__init__(name, level, live, weapon_name, weapon_power)
+        self.type_ally = type_ally
+
+characters = []
+
+def register_character(characters=characters):
+    print("Register Character: ")
+
+    character_type = input("What kind of character you want? (Allied or Enemy): ").strip().capitalize()
+    name_character = input("what name you want for your character?: ").strip().capitalize()
+    level = 1
+    live = 100
+    weapon_power = 10
+    if character_type == "Enemy":
+        enemy = Enemy(name_character, level, live, "Zombie","knife", weapon_power)
+        print(f"Enemy character '{name_character}' created successfully!")
+        print("---- Character Details ----")
+        print(f"Name: {enemy.name}")
+        print(f"Weapon Name: {enemy.weapon_name}")
+        print(f"Weapon Power: {enemy.weapon_power}")
+        print(f"Type: {enemy.type_enemy}")
+
+        characters.append(enemy)
+
+    else:
+        allied = Allied(name_character, level, live, "Knight", "sword", weapon_power)
+        print(f"Allied character '{name_character}' created successfully!")
+        print("---- Character Details ----")
+        print(f"Name: {allied.name}")
+        print(f"Weapon Name: {allied.weapon_name}")
+        print(f"Weapon Power: {allied.weapon_power}")
+        print(f"Type: {allied.type_ally}")
+
+        characters.append(allied)
+
+def attack_character():
+    target = input("Enter the name of the character to attack: ").capitalize().strip()
+
+    # Find the target character in the game
+    for character in characters:    
+        if character.name == target:
+            character.damage(10)  # Example damage value
+            print(f"Character {target} attacked successfully. Character's current health: {character.live}")
+            print("------------------------")
+            print("\n")
+        elif character.live <= 0:
+            print(f"Character {target} is already dead.")
+        elif character.name != target:
+            print(f"Character '{target}' not found.")
+
+def heal_character():
+    target = input("Enter the name of the character to heal: ").capitalize().strip()
+
+    # Find the target character in the game
+    for character in characters:    
+        if character.name == target:
+            character.healing(10)  # Example healing value
+            print(f"Character {target} healed successfully. Character's current health: {character.live}")
+            print("------------------------")
+            print("\n")
+        elif character.live <= 0:
+            print(f"Character {target} is dead and cannot be healed.")
+        elif character.name != target:
+            print(f"Character '{target}' not found.")
+
+def level_up_character():
+    target = input("Enter the name of the character to level up: ").capitalize().strip()
+    for character in characters:
+        if character.name == target:
+            character.level += 1
+            print(f"Character '{target}' leveled up to level {character.level}!")
+            return
+    print(f"Character '{target}' not found.")
+
+def show_all_characters(characters=characters):
+    if not characters:
+        print("No characters registered.")
+        return
+    print("---- All Characters ----")
+    for character in characters:
+        if isinstance(character, Enemy):
+            specific_type = character.type_enemy
+        elif isinstance(character, Allied):
+            specific_type = character.type_ally
+        print(f"Name: {character.name}, Type: {specific_type}, Level: {character.level}, Health: {character.live}, Weapon: {character.weapon_name}, Power: {character.weapon_power}")
+        print("------------------------")
+        print("\n")
+
+def mostrar_menu():
+    print(" ------------ MINIGAME------------- ")
+    print(" 1. Register your Character")
+    print(" 2. Attack Allied/Enemy")
+    print(" 3. Heal Allied/Enemy")
+    print(" 4. Level up Allied/Enemy")
+    print(" 5. Show all characters")
+    print(" 0. Salir")
+
+def main():
+  #Funcion principal
+  while True:
+    mostrar_menu()
+    opcion = input("Ingrese la opcion de 0 a 7: ").strip()
+    
+    if opcion == "1":
+      register_character()
+    elif opcion == "2":
+      attack_character()
+    elif opcion == "3":
+        heal_character()
+    elif opcion == "4":
+        level_up_character()
+    elif opcion == "5":
+      show_all_characters()
+    elif opcion == "0":
+      print("Session ended, goodbye!")
+      break
+
+if __name__ == "__main__":    
+    main()
+
+
